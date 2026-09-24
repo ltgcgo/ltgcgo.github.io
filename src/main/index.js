f8c3b36f1b82cb8b542a0e1442ebf3d6a71acc92
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-09-24 11:26:55 +0000

    Added docs.

--- src/main/index.js
+++ src/main/index.js
@@ -2,6 +2,63 @@
 
 import {Alpine} from "../../libs/alpine@alpinejs/alpine.min.js";
 
+/** @type {Map<string,Map<string,string>>} */
+const sources = new Map();
+/** @type {Map<string,string>|null} */
+let matchedSource;
+/** @param {string} text
+* @returns {string} */
+const getAdaptedUrl = function (text) {
+	if (typeof text !== "string") {
+		return text;
+	};
+	if (matchedSource) {
+		let result = text;
+		const segments = text.split("/");
+		const matchers = [`//${segments[2]}/`, `${segments[0]}//${segments[2]}/`];
+		for (const matcher of matchers) {
+			const target = matchedSource.get(matcher);
+			if (Number.isSafeInteger(target?.length)) {
+				result = result.replace(matcher, target);
+				break;
+			};
+		};
+		return result;
+	} else {
+		return text;
+	};
+};
+
+{
+	const sourceNoop = new Map();
+	sources.set("localhost", sourceNoop);
+	sources.set("127.0.0.1", sourceNoop);
+	const sourceDerive = new Map([["https://kb.ltgc.cc/","./kb/"]]);
+	sources.set("", sourceDerive);
+	sources.set("onion", sourceDerive);
+	sources.set("i2p", sourceDerive);
+	const sourceDefault = new Map([]);
+	sources.set("gh.ltgc.cc", sourceDefault);
+};
+{
+	const segments = location.hostname.split(".");
+	let matcher = "";
+	for (let i = 0; i <= segments.length; i ++) {
+		if (i > 0) {
+			if (i > 1) {
+				matcher = "." + matcher;
+			};
+			matcher = segments[segments.length - i] + matcher;
+		};
+		const matchTest = sources.get(matcher);
+		if (matchTest) {
+			matchedSource = matchTest;
+		} else {
+			break;
+		};
+	};
+	console.debug(`Used replacement matcher for: "${matcher}".`);
+};
 (async () => {
 	Alpine.store("catalogue", []);
 	const catalogueEntry = Alpine.store("catalogue");
@@ -11,11 +68,14 @@ import {Alpine} from "../../libs/alpine@alpinejs/alpine.min.js";
 			show = true;
 		};
 		if (show) {
+			if (e.docs?.kb) e.docs.kb = getAdaptedUrl(e.docs.kb);
 			catalogueEntry.push(e);
 		};
 	};
 })();
-
 (async () => {
 	Alpine.start();
+	for (const e of document.querySelectorAll("a.adaptive-link")) {
+		e.href = getAdaptedUrl(e.href);
+	};
 })();
